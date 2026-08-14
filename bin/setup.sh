8a604df50867769740cdb07e8fd0abb8b9685813
Glenn Oppegard <oppegard@gmail.com>
2026-08-13 18:12:29 -0600

    remove noise

--- bin/setup.sh
+++ bin/setup.sh
@@ -54,10 +54,6 @@ done
 cd "$DOTFILES_DIR/mise"
 mise bootstrap
 
-if [ "$__os" = "Darwin" ]; then
-  mise bootstrap macos defaults status
-fi
-
 DOTFILES_WORK_DIR="${DOTFILES_DIR}-work"
 if [ -d "$DOTFILES_WORK_DIR" ]; then
   echo
